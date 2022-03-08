7f2e53e1cd63c727976c086a4b181d202cff109d
Steven Essam <stevenessams7@gmail.com>
2022-03-08 13:44:41 +0100

    First Commit

---
+++ app/src/main/java/projet/projet_android_imdb/MainActivity.java
@@ -0,0 +1,27 @@
+package projet.projet_android_imdb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void SearchButton(View v) {
+
+        EditText editText1 = findViewById(R.id.textEntree);
+        Intent intent = new Intent(MainActivity.this, MovieSearch.class);
+        String data= editText1.getText().toString();
+        intent.putExtra("movieName",data);
+        startActivity(intent);
+
+    }
+}
---
+++ app/src/main/java/projet/projet_android_imdb/MovieSearch.java
@@ -0,0 +1,123 @@
+package projet.projet_android_imdb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class MovieSearch extends AppCompatActivity {
+
+    String data;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_search);
+        afficheData();
+        RequestTask rT =new RequestTask();
+        rT.execute("avengers");
+
+    }
+
+    public void checkAnswers(View v){
+
+
+
+    }
+
+
+    public void afficheData(){
+        Intent i = getIntent();
+        data =i.getStringExtra("data");
+
+    }
+
+
+    private class RequestTask extends AsyncTask <String, Void, String>  {
+
+
+
+        private String requete(String name) {
+            String response = "";
+            try {
+                HttpURLConnection connection = null;
+                URL url = new
+                        URL("https://imdb-api.com/en/API/SearchMovie/k_dgd1pq04/"+ URLEncoder.encode(name,"utf-8"));
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                InputStream inputStream = connection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String ligne = bufferedReader.readLine() ;
+                while (ligne!= null){
+                    response+=ligne;
+                    ligne = bufferedReader.readLine();
+                }
+                JSONObject toDecode = new JSONObject(response);
+                //   response = decodeJSON(toDecode);
+            } catch (UnsupportedEncodingException e) {
+                response = "problème d'encodage";
+            } catch (MalformedURLException e) {
+                response = "problème d'URL ";
+            } catch (IOException e) {
+                response = "problème de connexion ";
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return response;
+        }
+
+        private String decodeJSON(JSONObject jso) throws Exception {
+            String response = "";
+            return response;
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String response = requete(strings[0]);
+            return response;
+
+        }
+
+
+        protected void onPostExecute(String result) {
+
+            TextView tv = findViewById(R.id.tView);
+
+            tv.setText(result);
+
+        }
+
+    }
+
+    private String decodeJSON(JSONObject jso) throws Exception {
+        String response = "";
+
+            JSONArray jsoresult = jso.getJSONArray("results");
+            for (int i = 0; i < jsoresult.length(); i++)
+                response += jsoresult.getJSONObject(i).getString("title")+ " "
+                        +jsoresult.getJSONObject(i).getString("description")+" "
+//l image a metre tous seul
+
+                        +jsoresult.getJSONObject(i).getString("image");
+
+        return response;
+    }
+
+
+}
